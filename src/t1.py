19e16d785cab87e01d8c25f3bae346dceadbc132
Tom Soulanille <soul@prama.com>
2019-03-06 22:58:53 -0800

    t1.py: recurring_event(), recurring_bill()

--- src/t1.py
+++ src/t1.py
@@ -58,14 +58,48 @@ class Ledger:
         return self.cash
 
 
+# A financial can be run through
+
+class NameMe:
+    # The rules and initial conditions
+    def __init__(self, env):
+        self.env = env
+        self.bills = []
+
+
 class Scenario:
+    # exogenous events
     def __init__(self, env, ledger):
         self.env = env
         self.ledger = ledger
 
 
+def recurring_event(env, start, interval='monthly'):
+    tu = TimeUtil(env)
+    if interval == 'monthly':
+        waiter = tu.for_a_month
+    elif interval == 'weekly':
+        waiter = tu.for_a_week
+    else:
+        raise(ValueError, 'unrecognized interval')
+    yield env.timeout(start - env.now)
+    while True:
+        yield waiter()
+
+
+def recurring_bill(env, start, name, amount, interval='monthly'):
+    if isinstance(amount, numbers.Number):
+        lamount = lambda e: amount
+    else:
+        lamount = amount
+    for e in recurring_event(env, start, interval):
+        yield e
+        print("{}: {} bill ${}".format(isodate(env.now), name,
+                                       lamount(env)))
+
+
 class Recurring_Bill:
-    def __init__(self, env, name, start, amount, interval='monthly'):
+    def __init__(self, env, start, name, amount, interval='monthly'):
         self.env = env
         self.name = name
         self.start = start
@@ -95,12 +129,15 @@ class Recurring_Bill:
 
 
 def setup(env):
-    rwb = Recurring_Bill(env, 'water', simdate('2019-04-15'), 123.45)
-    reb = Recurring_Bill(env, 'electric', simdate('2019-04-10'),
+    rwb = recurring_bill(env, simdate('2019-04-15'), 'water', 123.45)
+    env.process(rwb)
+    reb = recurring_bill(env, simdate('2019-04-10'), 'electric',
                          lambda e: round(random.gauss(100, 30), 2))
-    rgb = Recurring_Bill(env, 'groceries', simdate('2019-04-02'),
+    env.process(reb)
+    rgb = recurring_bill(env, simdate('2019-04-02'), 'groceries',
                          lambda e: round(random.gauss(60, 10), 2),
                          interval='weekly')
+    env.process(rgb)
 
 
 def main():
@@ -111,4 +148,5 @@ def main():
     print(isodate(env.now))
 
 if __name__ == '__main__':
+    random.seed(0)
     main()
