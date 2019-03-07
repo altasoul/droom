1115e3e82a5537d39de44a8b2340543664241f8b
Tom Soulanille <soul@prama.com>
2019-03-06 22:02:11 -0800

    t1.py: monthly, weekly, more bills

--- src/t1.py
+++ src/t1.py
@@ -1,6 +1,7 @@
 
 import datetime
 import numbers
+import pdb
 import random
 import simpy
 #import time
@@ -16,7 +17,7 @@ def simdate(s):
 def isodate(x):
     return datetime.date.fromordinal(int(x)).isoformat()
 
-def nextmonth(x):
+def next_month(x):
     d = datetime.date.fromordinal(int(x))
     if d.month < 12:
         dnext = d.replace(month=d.month+1)
@@ -24,11 +25,44 @@ def nextmonth(x):
         dnext = d.replace(year=d.year+1, month=1)
     return dnext.toordinal()
 
+def next_week(x):
+    return x + 7
+
+
+
+class TimeUtil:
+    def __init__(self, env):
+        self.env = env
+
+    def for_a_month(self):
+        now = self.env.now
+        return self.env.timeout(next_month(now)-now)
+
+    def for_a_week(self):
+        now = self.env.now
+        return self.env.timeout(next_week(now)-now)
+
+
+class Ledger:
+    # very temporary placeholder
+    def __init__(self):
+        self.cash = 0
+
+    def devit(self, amt):
+        self.cash += amt        # To accountants Cash is a Debit account
+
+    def credit(self, amt):
+        self.cash -= amt
+
+    def balance(self):
+        return self.cash
 
 
 class Scenario:
-    def __init__(self, env):
+    def __init__(self, env, ledger):
         self.env = env
+        self.ledger = ledger
+
 
 class Recurring_Bill:
     def __init__(self, env, name, start, amount, interval='monthly'):
@@ -44,18 +78,29 @@ class Recurring_Bill:
 
     def run(self):
         env = self.env
-        yield env.timeout(self.start - env.now)
+        tu = TimeUtil(env)
+        #yield env.timeout(self.start - env.now)
+        t = env.timeout(self.start - env.now)
+        print(repr(t.callbacks))
+        t.callbacks.append(lambda e: print('callback from', e))
         while True:
             amt = self.lamount(env)
             print("{}: {} bill ${}".format(isodate(env.now), self.name, amt))
-            yield self.env.timeout(nextmonth(env.now)-env.now)
+            #yield self.env.timeout(next_month(env.now)-env.now)
+            if self.interval == 'monthly':
+                yield tu.for_a_month()
+            elif self.interval == 'weekly':
+                yield tu.for_a_week()
 
 
 
 def setup(env):
     rwb = Recurring_Bill(env, 'water', simdate('2019-04-15'), 123.45)
-    reb = Recurring_Bill(env, 'electric', simdate('2019-04-15'),
+    reb = Recurring_Bill(env, 'electric', simdate('2019-04-10'),
                          lambda e: round(random.gauss(100, 30), 2))
+    rgb = Recurring_Bill(env, 'groceries', simdate('2019-04-02'),
+                         lambda e: round(random.gauss(60, 10), 2),
+                         interval='weekly')
 
 
 def main():
